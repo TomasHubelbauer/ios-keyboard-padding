3b6cf8954a4bd99803a616fb9510ee286e996b81
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-03-15 12:10:20 +0100

    Update iOS platform version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "KeyboardAwareModifier",
+    platforms: [.iOS(.v13)]
     products: [
         .library(name: "KeyboardAwareModifier", targets: ["KeyboardAwareModifier"]),
     ],
